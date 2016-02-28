a91b63434c29d9b7de13f488bce32d9b25d7165e
Generic7 <qoo0302@naver.com>
2016-02-28 20:47:07 +0900

    Basis of PhoneBook Person datas.

---
+++ PhoneBook/src/kr/dothome/generic7/project/PhoneBookData/PhoneBookPersonData.java
@@ -0,0 +1,40 @@
+package kr.dothome.generic7.project.PhoneBookData;
+
+public abstract class PhoneBookPersonData
+{
+	private int age;						// a person's age
+	private String name;					// a person's name
+	private int phoneNum;					// a person's phone number
+	private boolean genderidentifier;		// 'true' means "male", 'false' means "female"
+	
+	public PhoneBookPersonData(int age, String name, int phoneNum)
+	{
+		this.age = age;
+		this.name = name;
+		this.phoneNum = phoneNum;
+	}
+	
+	public PhoneBookPersonData(int age, String name, int phoneNum, boolean gender)
+	{
+		this.age = age;
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.genderidentifier = gender;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+	
+	public boolean getGender() {
+		return genderidentifier;
+	}
+}
